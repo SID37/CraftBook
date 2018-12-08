c720e602831b5483086981dea7fd547715b8d072
SID37 <33577875+SID37@users.noreply.github.com>
2018-12-09 00:09:53 +0300

    Предварительное описание модели

---
+++ CraftBook/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftBook.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instruction { get; set; }
+        public byte[] Image { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
