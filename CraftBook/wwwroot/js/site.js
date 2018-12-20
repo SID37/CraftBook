70d0ecedcc7e4be2ab543dbe61db082775949257
Viacheslav Ivanchenko <slavikelk@yandex.ru>
2018-12-20 21:23:40 +0300

    Начата работа над инвентарём.

--- CraftBook/wwwroot/js/site.js
+++ CraftBook/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function createIngredient(name, count) {
+    var ing = document.createElement('li');
+    ing.appendChild(document.createElement('fieldset'));
+    ing = ing.firstChild;
+    const fname = document.createElement('output');
+    const fcount = document.createElement('output');
+    fname.appendChild(document.createTextNode(name));
+    fcount.appendChild(document.createTextNode(count));
+    ing.appendChild(fname);
+    ing.appendChild(fcount);
+    return ing;
+}
+function Inventory() {
+    inputNameIngr = document.querySelector("article.inventory input[type=\"text\"]");
+    inputCountIngr = document.querySelector("article.inventory input[type=\"number\"]");
+    buttonAddIngridient = document.querySelector("article.inventory input[type=\"button\"]");
+    listIngridients = document.querySelector("article.inventory ul");
+    function addIngredient() {
+        listIngridients.appendChild(createIngredient(inputNameIngr.value, inputCountIngr.value));
+        inputCountIngr.value = null;
+        inputNameIngr.value = null;
+    }
+    buttonAddIngridient.onclick = addIngredient;
+}
+var inventory = new Inventory();
