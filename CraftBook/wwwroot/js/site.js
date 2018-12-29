0ce50290f0f73981a5160861e4b3b88f621069a8
Viacheslav Ivanchenko <slavikelk@yandex.ru>
2018-12-29 23:06:43 +0300

    Приём ajax запроса с ингредиентом на стороне клиента

--- CraftBook/wwwroot/js/site.js
+++ CraftBook/wwwroot/js/site.js
@@ -2,28 +2,47 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
 function createIngredient(name, count) {
-    var ing = document.createElement('li');
-    ing.appendChild(document.createElement('fieldset'));
+    var ing = document.createElement('div');
+    ing.setAttribute('class', 'fieldform')
+    ing.appendChild(document.createElement('div'));
     ing = ing.firstChild;
-    const fname = document.createElement('output');
-    const fcount = document.createElement('output');
-    fname.appendChild(document.createTextNode(name));
-    fcount.appendChild(document.createTextNode(count));
+    ing.setAttribute('class', 'in-frame');
+    const fname = document.createElement('input');
+    fname.setAttribute('type', 'text');
+    const fcount = document.createElement('input');
+    fcount.setAttribute('type', 'number');
+    fname.setAttribute('value',name);
+    fcount.setAttribute('value', count);
     ing.appendChild(fname);
     ing.appendChild(fcount);
     return ing;
 }
 function Inventory() {
+    xhr = new XMLHttpRequest();
     inputNameIngr = document.querySelector("article.inventory input[type=\"text\"]");
     inputCountIngr = document.querySelector("article.inventory input[type=\"number\"]");
-    buttonAddIngridient = document.querySelector("article.inventory input[type=\"button\"]");
-    listIngridients = document.querySelector("article.inventory ul");
+    form = document.querySelector("article.inventory form.create-ingredient");
+    listIngridients = document.querySelector("article.inventory form.list-ingredients");
     function addIngredient() {
-        listIngridients.appendChild(createIngredient(inputNameIngr.value, inputCountIngr.value));
-        inputCountIngr.value = null;
-        inputNameIngr.value = null;
+        xhr.open("POST", "IngredientQuant/FindName", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onloadend = function (event) {
+            //todo нормально как-то
+            if (xhr.status === 404) {
+                alert('ингредиент не найден!');
+                return;
+            }
+            listIngridients.insertAdjacentHTML("beforeend", xhr.response);
+            inputCountIngr.value = null;
+            inputNameIngr.value = null;
+        };
+        xhr.send("ingredientName=" + encodeURIComponent(inputNameIngr.value) + "&volume=" +encodeURIComponent(inputCountIngr.value));
+        return false;
     }
-    buttonAddIngridient.onclick = addIngredient;
+    form.onsubmit = addIngredient;
 }
+
 var inventory = new Inventory();
+
