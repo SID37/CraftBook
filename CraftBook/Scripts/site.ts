ef8f80d934c4ffcb7637134d4db0303c1087508c
Viacheslav Ivanchenko <slavikelk@yandex.ru>
2019-01-05 01:15:14 +0300

    Переход к ООП в стиле TS Удаление ингредиентов по кнопке Сокрытие кнопки добавления при некорректном вводе

--- CraftBook/Scripts/site.ts
+++ CraftBook/Scripts/site.ts
@@ -6,69 +6,99 @@ function Request(address) {
     setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 }*/
 
-function Inventory() {
-    var inputNameIngr = document.querySelector("article.inventory input[type=\"text\"]") as HTMLInputElement;
-    var inputCountIngr = document.querySelector("article.inventory input[type=\"number\"]") as HTMLInputElement;
-    var inputUIIngr = document.querySelector("article.inventory input[name=\"ingredient_unit\"]") as HTMLInputElement;
-    var form = document.querySelector("article.inventory form.create-ingredient") as HTMLFormElement;
-    var listIngridients = document.querySelector("article.inventory form.list-ingredients");
-    //Подсказки при вводе
-    inputNameIngr.addEventListener("input",
-        () => {
+class Inventory {
+    //requestAdd: XMLHttpRequest;
+    inputNameIngr: HTMLInputElement;
+    inputButton: HTMLInputElement;
+    inputCountIngr: HTMLInputElement;
+    inputUIIngr: HTMLInputElement;
+    form: HTMLFormElement;
+    listIngridients: HTMLElement;
 
-            var nameChip = inputNameIngr.value;
-            console.log(nameChip);
-            if (nameChip.length === 0)
-                return;
-            var requestSearch = new XMLHttpRequest();
-            requestSearch.open("POST", "/Ingredients/Index", true);
-            requestSearch.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            requestSearch.onloadend = () => {
-                if (requestSearch.status === 404)
+
+    constructor() {
+        this.inputNameIngr = document.querySelector("article.inventory input[type=\"text\"]") as HTMLInputElement;
+        this.inputButton = document.getElementById("buttonAddIngridient") as HTMLInputElement;
+        this.inputCountIngr = document.querySelector("article.inventory input[type=\"number\"]") as HTMLInputElement;
+        this.inputUIIngr =
+            document.querySelector("article.inventory input[name=\"ingredient_unit\"]") as HTMLInputElement;
+        this.form = document.querySelector("article.inventory form.create-ingredient") as HTMLFormElement;
+        this.listIngridients = ((document.querySelector("article.inventory form.list-ingredients")) as HTMLElement);
+
+
+        //Подсказки при вводе
+        this.inputNameIngr.addEventListener("input",
+            () => {
+
+                var nameChip = this.inputNameIngr.value;
+                console.log(nameChip);
+                if (nameChip.length === 0)
                     return;
-                var tmp = document.querySelector("datalist[id=\"ingredients\"");
-                if (tmp != null)
-                    tmp.parentNode.removeChild(tmp);
-                form.insertAdjacentHTML("afterend", requestSearch.response);
-            }
-            requestSearch.send("nameChip=" + encodeURIComponent(nameChip));
-        });
-    //Окончание ввода названия ингредиента - устанавливаем единицы измерения
-    inputNameIngr.addEventListener("change",
-        () => {
-            var nameChip = inputNameIngr.value;
-            var tmp = document.querySelector("option[value=\"" + nameChip + "\"");
-            if (tmp == null)
-                return;
-            inputUIIngr.setAttribute("value", tmp.getAttribute("label"));
-        });
+                var requestSearch = new XMLHttpRequest();
+                requestSearch.open("POST", "/Ingredients/Index", true);
+                requestSearch.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                requestSearch.onloadend = () => {
+                    if (requestSearch.status === 404)
+                        return;
+                    var tmp = document.querySelector("datalist[id=\"ingredients\"");
+                    if (tmp != null)
+                        tmp.parentNode.removeChild(tmp);
+                    this.form.insertAdjacentHTML("afterend", requestSearch.response);
+                }
+                requestSearch.send("nameChip=" + encodeURIComponent(nameChip));
+            });
+        //Окончание ввода названия ингредиента - устанавливаем единицы измерения
+        this.inputNameIngr.addEventListener("change",
+            () => {
+                var nameChip = this.inputNameIngr.value;
+                var tmp = document.querySelector("option[value=\"" + nameChip + "\"");
+                if (tmp == null) {
+                    this.inputUIIngr.value = null;
+                    this.inputButton.style.visibility= "hidden";
+                } else {
+                    this.inputUIIngr.value = tmp.getAttribute("label");
+                    this.inputButton.style.visibility = "visible";
+                }
+            });
 
-    var requestAdd = new XMLHttpRequest();
-    function addIngredient() {
-        requestAdd.open("POST", "/IngredientQuant/FindName", true);
-        requestAdd.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        requestAdd.onloadend = () => {
-            //todo нормально как-то
-            if (requestAdd.status === 404) {
-                alert('ингредиент не найден!');
-                return;
-            }
-            listIngridients.insertAdjacentHTML("beforeend", requestAdd.response);
-            let fieldform = form.lastChild;
-            console.log(fieldform);
-            (fieldform.lastChild as HTMLInputElement).onkeyup = () => {
-                    form.removeChild(fieldform);
+        this.form.onsubmit = (event: Event) => {
+            try {
+                if (this.inputUIIngr.value === "") {
                     return false;
+                }
+                var requestAdd = new XMLHttpRequest();
+                requestAdd.open("POST", "/IngredientQuant/FindName", true);
+                requestAdd.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                requestAdd.onloadend = () => {
+                    //todo нормально как-то
+                    if (requestAdd.status === 404) {
+                        alert('ингредиент не найден!');
+                        return;
+                    }
+                    this.listIngridients.insertAdjacentHTML("beforeend", requestAdd.response);
+                    const fieldform = this.listIngridients.lastChild as HTMLElement;
+                    (fieldform.querySelector("input[type=\"image\"") as HTMLInputElement).onclick = () => {
+                        fieldform.remove();
+                        return false;
+                    };
+                    this.inputCountIngr.value = null;
+                    this.inputNameIngr.value = null;
+                    this.inputUIIngr.value = null;
                 };
-            inputCountIngr.setAttribute("value",  null);
-            inputNameIngr.setAttribute("value",  null);
-            inputUIIngr.setAttribute("value", null);
-        };
-        requestAdd.send("ingredientName=" + encodeURIComponent(inputNameIngr.value) + "&volume=" +encodeURIComponent(inputCountIngr.value));
-        return false;
-    }
+                const name = this.inputNameIngr.value;
+                const count = this.inputCountIngr.value;
+                requestAdd.send("ingredientName=" +
+                    encodeURIComponent(name) +
+                    "&volume=" +
+                    encodeURIComponent(count));
 
-    form.onsubmit = addIngredient;
+            } catch (e) {
+                console.log(e.toString());
+                return false;
+            }
+            return false;
+        }
+    }
 }
 
 var inventory = new Inventory();
--- CraftBook/wwwroot/js/site.js
+++ CraftBook/wwwroot/js/site.js
@@ -4,63 +4,86 @@ function Request(address) {
     open("POST", address, true);
     setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 }*/
-function Inventory() {
-    var inputNameIngr = document.querySelector("article.inventory input[type=\"text\"]");
-    var inputCountIngr = document.querySelector("article.inventory input[type=\"number\"]");
-    var inputUIIngr = document.querySelector("article.inventory input[name=\"ingredient_unit\"]");
-    var form = document.querySelector("article.inventory form.create-ingredient");
-    var listIngridients = document.querySelector("article.inventory form.list-ingredients");
-    //Подсказки при вводе
-    inputNameIngr.addEventListener("input", function () {
-        var nameChip = inputNameIngr.value;
-        console.log(nameChip);
-        if (nameChip.length === 0)
-            return;
-        var requestSearch = new XMLHttpRequest();
-        requestSearch.open("POST", "/Ingredients/Index", true);
-        requestSearch.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        requestSearch.onloadend = function () {
-            if (requestSearch.status === 404)
+var Inventory = /** @class */ (function () {
+    function Inventory() {
+        var _this = this;
+        this.inputNameIngr = document.querySelector("article.inventory input[type=\"text\"]");
+        this.inputButton = document.getElementById("buttonAddIngridient");
+        this.inputCountIngr = document.querySelector("article.inventory input[type=\"number\"]");
+        this.inputUIIngr =
+            document.querySelector("article.inventory input[name=\"ingredient_unit\"]");
+        this.form = document.querySelector("article.inventory form.create-ingredient");
+        this.listIngridients = (document.querySelector("article.inventory form.list-ingredients"));
+        //Подсказки при вводе
+        this.inputNameIngr.addEventListener("input", function () {
+            var nameChip = _this.inputNameIngr.value;
+            console.log(nameChip);
+            if (nameChip.length === 0)
                 return;
-            var tmp = document.querySelector("datalist[id=\"ingredients\"");
-            if (tmp != null)
-                tmp.parentNode.removeChild(tmp);
-            form.insertAdjacentHTML("afterend", requestSearch.response);
-        };
-        requestSearch.send("nameChip=" + encodeURIComponent(nameChip));
-    });
-    //Окончание ввода названия ингредиента - устанавливаем единицы измерения
-    inputNameIngr.addEventListener("change", function () {
-        var nameChip = inputNameIngr.value;
-        var tmp = document.querySelector("option[value=\"" + nameChip + "\"");
-        if (tmp == null)
-            return;
-        inputUIIngr.setAttribute("value", tmp.getAttribute("label"));
-    });
-    var requestAdd = new XMLHttpRequest();
-    function addIngredient() {
-        requestAdd.open("POST", "/IngredientQuant/FindName", true);
-        requestAdd.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        requestAdd.onloadend = function () {
-            //todo нормально как-то
-            if (requestAdd.status === 404) {
-                alert('ингредиент не найден!');
-                return;
-            }
-            listIngridients.insertAdjacentHTML("beforeend", requestAdd.response);
-            var fieldform = form.lastChild;
-            console.log(fieldform);
-            fieldform.lastChild.onkeyup = function () {
-                form.removeChild(fieldform);
-                return false;
+            var requestSearch = new XMLHttpRequest();
+            requestSearch.open("POST", "/Ingredients/Index", true);
+            requestSearch.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            requestSearch.onloadend = function () {
+                if (requestSearch.status === 404)
+                    return;
+                var tmp = document.querySelector("datalist[id=\"ingredients\"");
+                if (tmp != null)
+                    tmp.parentNode.removeChild(tmp);
+                _this.form.insertAdjacentHTML("afterend", requestSearch.response);
             };
-            inputCountIngr.setAttribute("value", null);
-            inputNameIngr.setAttribute("value", null);
-            inputUIIngr.setAttribute("value", null);
+            requestSearch.send("nameChip=" + encodeURIComponent(nameChip));
+        });
+        //Окончание ввода названия ингредиента - устанавливаем единицы измерения
+        this.inputNameIngr.addEventListener("change", function () {
+            var nameChip = _this.inputNameIngr.value;
+            var tmp = document.querySelector("option[value=\"" + nameChip + "\"");
+            if (tmp == null) {
+                _this.inputUIIngr.value = null;
+                _this.inputButton.style.visibility = "hidden";
+            }
+            else {
+                _this.inputUIIngr.value = tmp.getAttribute("label");
+                _this.inputButton.style.visibility = "visible";
+            }
+        });
+        this.form.onsubmit = function (event) {
+            try {
+                if (_this.inputUIIngr.value === "") {
+                    return false;
+                }
+                var requestAdd = new XMLHttpRequest();
+                requestAdd.open("POST", "/IngredientQuant/FindName", true);
+                requestAdd.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                requestAdd.onloadend = function () {
+                    //todo нормально как-то
+                    if (requestAdd.status === 404) {
+                        alert('ингредиент не найден!');
+                        return;
+                    }
+                    _this.listIngridients.insertAdjacentHTML("beforeend", requestAdd.response);
+                    var fieldform = _this.listIngridients.lastChild;
+                    fieldform.querySelector("input[type=\"image\"").onclick = function () {
+                        fieldform.remove();
+                        return false;
+                    };
+                    _this.inputCountIngr.value = null;
+                    _this.inputNameIngr.value = null;
+                    _this.inputUIIngr.value = null;
+                };
+                var name_1 = _this.inputNameIngr.value;
+                var count = _this.inputCountIngr.value;
+                requestAdd.send("ingredientName=" +
+                    encodeURIComponent(name_1) +
+                    "&volume=" +
+                    encodeURIComponent(count));
+            }
+            catch (e) {
+                console.log(e.toString());
+                return false;
+            }
+            return false;
         };
-        requestAdd.send("ingredientName=" + encodeURIComponent(inputNameIngr.value) + "&volume=" + encodeURIComponent(inputCountIngr.value));
-        return false;
     }
-    form.onsubmit = addIngredient;
-}
+    return Inventory;
+}());
 var inventory = new Inventory();
