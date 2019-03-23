e8df91d8d84c948ca58578cbb74f8ff01600de8e
Viacheslav Ivanchenko <slavikelk@yandex.ru>
2019-03-23 22:11:06 +0300

    FavoriteMarkView - отвечает за отображение и реагирование значка флажка

---
+++ CraftBook/Scripts/Classes/listInStorage.ts
@@ -0,0 +1,21 @@
+﻿class ListInStorage<T> {
+    constructor(name: string, temporary: boolean) {
+        this.key = name;
+        this.storage = temporary ? sessionStorage : localStorage;
+        this.list = JSON.parse(this.storage.getItem(this.key)) as Array<T>;
+        if (this.list == null)
+            this.list = new Array<T>();
+        window.addEventListener("unload", () => { this.storage.setItem(this.key, JSON.stringify(this.list)); });
+    }
+    private key: string;
+    private storage: Storage;
+    private list: Array<T>;
+
+    getList(): Array<T> {
+        return this.list;
+    }
+
+    setList(list: Array<T>) {
+        this.list = list;
+    }
+}
--- CraftBook/Scripts/Classes/listRecipe.ts
+++ CraftBook/Scripts/Classes/listRecipe.ts
@@ -26,6 +26,13 @@ class ListRecipes {
             }
         });
 
+        this.headNode.querySelectorAll("article.recipe-preview").forEach((recipe:
+            HTMLDivElement) => {
+            let id = parseInt(recipe.id.substr(1));
+            new FavoriteMarkView(recipe.querySelector("section > header > img") as HTMLImageElement)
+                .onChangeMode = (m) => { console.log(m+" "+id); };
+        });
+
         //TODO так как теперь вместо img используется div с фоновым изображением, мы потеряли событие error
         /*this.headNode.querySelectorAll(".recipe_avatar").forEach((div: HTMLDivElement) => {
             div.addEventListener("error",
@@ -37,5 +44,48 @@ class ListRecipes {
 
     constructor(node: HTMLElement) {
         this.headNode = node;
+    }
+}
+
+class FavoriteMarkView {
+    private img: HTMLImageElement;
+    private mode: boolean;
+    onChangeMode: (mode: boolean) => void;
+
+    constructor(node: HTMLImageElement, mode: boolean = false) {
+        this.img = node;
+        this.setMode(mode);
+        this.img.onclick = () => {
+            this.setMode(!this.mode);
+        };
+    }
+
+    setMode(mode: boolean) {
+        this.mode = mode;
+        if (mode)
+            this.img.src = "/images/bookmark-active.svg";
+        else 
+            this.img.src = "/images/bookmark-passive.svg";
+        if (this.onChangeMode)
+            this.onChangeMode(mode);
+    }
+}
+
+class ListFavoriteRecipes
+{
+    private ids: Array<Number>;
+    private storage: ListInStorage<Number>;
+
+    constructor() {
+        this.storage = new ListInStorage<Number>("favoriteRecipes", false);
+        this.ids = this.storage.getList();
+    }
+
+    get() {
+        return this.ids;
+    }
+
+    add(id: Number) {
+        this.ids.push(id);
     }
 }
