958c166ccafbb80f042382ee91fb0fbdb53368b3
Viacheslav Ivanchenko <slavikelk@yandex.ru>
2019-03-25 16:39:10 +0300

    fix pageSize and preview height

--- CraftBook/Controllers/RecipesController.cs
+++ CraftBook/Controllers/RecipesController.cs
@@ -13,7 +13,7 @@ namespace CraftBook.Controllers
     public class RecipesController : Controller
     {
         private readonly CraftBookContext _context;
-        private static int PageSize = 50;
+        private static int PageSize = 10;
 
         /// <summary>
         /// Конструктор
