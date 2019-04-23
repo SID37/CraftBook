805c5826b11a7cdcd886b033fa06d4e7310e5e07
Viacheslav Ivanchenko <slavikelk@yandex.ru>
2019-04-23 12:33:05 +0300

    Added button for to pin avatar in creating recipe

--- CraftBook/Scripts/Classes/image.ts
+++ CraftBook/Scripts/Classes/image.ts
@@ -3,6 +3,45 @@ type UploadListener = (link: string) => void;
 
 class ImageUploader {
     constructor(node: HTMLElement, uploaded: UploadListener, mod: UploadMod = "one") {
+        (node.parentNode.querySelector('input[type="file"]') as HTMLInputElement).addEventListener("change", (ev)=> {
+            const data = ev.target as any;
+            if (data) {
+                if (data.files.length > 0) {
+                    ev.preventDefault();
+                    if (data.files.length > 1 && mod === "one") {
+                        error.display("Здесь можно загрузить только один файл!");
+                        return;
+                    }
+                }
+                console.group("files");
+                for (let i = 0; i < data.files.length; i++) {
+                    let file = data.files[i];
+                    if (file.type.match(/image/)) {
+                        let uploadRequest = new XMLHttpRequest();
+                        console.log(`Отловили файл ${file.name}`);
+                        uploadRequest.onloadend = () => {
+                            error.display(false);
+                            if (uploadRequest.status > 300) {
+                                //TODO как-то сообщить пользователю
+                                error.display(`Не удалось отправить картинку. Ошибка ${uploadRequest.status}.`);
+                                return;
+                            }
+                            let message = JSON.parse(uploadRequest.response);
+                            if (message.message) {
+                                error.display(message.message);
+                                return;
+                            }
+                            uploaded(message.link);
+                        }
+                        uploadRequest.open("POST", "/Images/Create", true);
+                        let formData = new FormData();
+                        formData.append("image", file);
+                        uploadRequest.send(formData);
+                    }
+                }
+                console.groupEnd();
+            }
+        });
         let error = new ErrorView(node);
         node.ondrop = (ev: DragEvent) => {
             const data = ev.dataTransfer;
